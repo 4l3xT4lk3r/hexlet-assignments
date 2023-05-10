b887c4e08a6a2b9e47616c30b9ff76228fdaa42d
@hexlet/cli <support@hexlet.io>
2023-05-11 02:38:34 +0400

    submit java-oop-ru/errors

---
+++ java-oop-ru/errors/src/main/java/exercise/App.java
@@ -0,0 +1,18 @@
+package exercise;
+
+// BEGIN
+public class App {
+
+    public static void printSquare(Circle circle){
+        try {
+
+            System.out.println(Math.round(circle.getSquare()));
+        }catch (NegativeRadiusException exception){
+            System.out.println(exception.getMessage());
+        }finally {
+            System.out.println("Вычисление окончено");
+        }
+
+    }
+}
+// END
